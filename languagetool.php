f2a2853727b3b60fcc00d6033257907f0de3cb7f
Martijn <martijn@inktweb.nl>
2016-04-08 18:07:20 +0200

    Add version number, some minor documentation, re-organize

--- languagetool.php
+++ languagetool.php
@@ -1,6 +1,9 @@
 <?php
 /**
  * Use the Public HTTP API of LanguageTool to check for spelling errors
+ *
+ *  @version v1.0.1
+ *  @license https://opensource.org/licenses/MIT
  */
 namespace MartijnOud;
 
@@ -14,13 +17,32 @@ class LanguageTool
         $this->langCode = $langCode;
     }
 
+    /**
+     * Make a call to the API
+     * @param string Text with (potential) spelling mistakes
+     * @return string Text with fixed spelling mistakes
+     */
+    public function check($strInput)
+    {
+        if (!empty($strInput)) {
 
-    // Make POST request to API
+            $xml = $this->call($strInput);
+            $strReplacement = $this->checkSpelling($xml);
+
+            return $strReplacement['return'];
+        }
+    }
+
+    /**
+     * Send POST request to API
+     * @param  string $strInput
+     * @return
+     */
     private function call($strInput)
     {
 
         $payload = 'language='.$this->langCode.'&text='.urlencode($strInput);
- 
+
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, "https://languagetool.org");
         curl_setopt($curl, CURLOPT_PORT, 8081);
@@ -30,12 +52,18 @@ class LanguageTool
         curl_setopt($curl, CURLOPT_POSTFIELDS, $payload);
         $response = curl_exec($curl);
         $xml = simplexml_load_string($response);
-    
+
         return $xml;
 
     }
 
-    private function cleanReplacement($strReplacement) {
+    /**
+     * [cleanReplacement description]
+     * @param  [type] $strReplacement [description]
+     * @return [type]                 [description]
+     */
+    private function cleanReplacement($strReplacement)
+    {
 
         if (strpos($strReplacement, '#') !== false) {
             $pieces = explode('#', $strReplacement);
@@ -45,6 +73,9 @@ class LanguageTool
         return $strReplacement;
     }
 
+    /**
+     * Call API and check for misspellings
+     */
     private function checkSpelling($xml)
     {
 
@@ -52,19 +83,19 @@ class LanguageTool
         foreach ($xml->error as $error) {
 
             if ($error['locqualityissuetype'] == "misspelling") {
-                
+
                 // Get first replacement
                 $strReplacement = (string) $error['replacements'];
                 $strReplacement = $this->cleanReplacement($strReplacement);
 
-                $strError = (string)$error['context'];
-                $strContext = (string)$error['context'];
+                $strError = (string) $error['context'];
+                $strContext = (string) $error['context'];
 
                 // handle replacement inside a full sentence
                 if (!empty($error['fromx']) AND !empty($error['tox'])) {
-                   
+
                     // get specific error word from complete sentence
-                    $fromX = (int)$error['fromx'];
+                    $fromX = (int) $error['fromx'];
                     $len = strlen($strReplacement);
                     $strError = substr($strContext, $fromX, $len);
 
@@ -72,7 +103,7 @@ class LanguageTool
 
                 // Add replacement to array
                 $arrReplacements[$strError] = $strReplacement;
-               
+
             }
 
         }
@@ -90,20 +121,4 @@ class LanguageTool
         );
     }
 
-    /**
-     * Make a call to the API
-     * @param string Text with (potential) spelling mistakes
-     * @return string Text with fixed spelling mistakes 
-     */
-    public function check($strInput)
-    {
-        if (!empty($strInput)) {
-
-            $xml = $this->call($strInput);
-            $strReplacement = $this->checkSpelling($xml);
-
-            return $strReplacement['return'];
-        }
-    }
-    
 }
